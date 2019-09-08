41a362e114de5db977dc215b2b9fcd067d9bfbaa
piyushmanglani08 <31417442+piyushmanglani08@users.noreply.github.com>
2019-09-08 14:35:36 +0530

    Add files via upload
    Configuration Done

---
+++ config.py
@@ -0,0 +1,24 @@
+'''
+configuration
+'''
+
+toxic_words = [
+	'asshole',
+	'douche',
+	'douchebag',
+	'moron',
+	'idiot',
+	'stupid',
+	'shit',
+	'schmuck',
+	'dickhead',
+	'jackass',
+	'ass',
+        'suck',
+        'dumbass',
+	'bastard',
+	'shithole',
+	'faggot',
+	'jerk',
+
+]
